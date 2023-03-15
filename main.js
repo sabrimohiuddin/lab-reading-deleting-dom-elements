9cc375fa2ad705a7b629a711b58ef3b42985261d
Sabri Mohiuddin <you@example.com>
2023-03-14 22:05:06 -0400

    Reading and Deleting DOM Elements Labd

---
+++ main.js
@@ -0,0 +1,12 @@
+
+document.querySelector("span").remove()
+document.querySelector("li").remove()
+document.querySelector(".posts article").remove()
+
+
+ //document.querySelectorAll('.post title').remove();
+
+  //document.querySelectorAll('.posts .post h3').forEach(title => title.remove());
+
+
+  document.querySelectorAll('.posts article h3').forEach(title => title.remove());
